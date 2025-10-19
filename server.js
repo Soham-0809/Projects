a3efcdbe773ee2c2054a16bc92412e65f3f95563
Soham-0809 <sohamnraorane@gmail.com>
2025-10-19 20:35:32 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express=require('express');
+const app=express();
+const port=process.env.PORT || 3000;    
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+});
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on ${process.env.PORT}`);
+});
